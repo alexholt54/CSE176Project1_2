385669b4f7fa79082648ce1365c204c395f6e2dc
RyanSaini <61637919+RyanSaini@users.noreply.github.com>
2021-12-09 03:45:29 -0800

    Update fonts-XGB.py

--- fonts-XGB.py
+++ fonts-XGB.py
@@ -4,13 +4,45 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
+import os
+import glob
+import csv
 
 def main():
 
     # Data reading will go here...
+    # 200 images from each file ~30k
+    #70 for validation/testing
+
+    
+    """     myData = pd.read_csv("datasets/fonts/AGENCY.csv")
+    tempdf = pd.DataFrame(myData)
+    print(tempdf)
+    tempdf = tempdf.drop(['orientation', 'm_top', 'm_left', 'originalH', 'originalW', 'h', 'w', 'fontVariant'], axis=1)
+    print(tempdf) """ 
+
+
+    #mainDF = pd.DataFrame()
+
+"""     for f in csv_files:
+        df = pd.read_csv(f)
+        #print('Location:', f)
+        print('File Name:', f.split("\\")[-1])
+        df = df.drop(['orientation', 'm_top', 'm_left', 'originalH', 'originalW', 'h', 'w', 'fontVariant'], axis=1)
+        #print(df)
+        mainDF = mainDF.append(df)
+
+    print("/n")
+    print(mainDF) """
+
+    #train_data = pd.read_csv("datasets/fonts/AGENCY.csv")
+    #test_data = pd.read_csv("")
+
 
     # Change parameters here
-    model = xgb.XGBClassifier()
+    # Model = xgb.XGBClassifier()
+
+
 
 if __name__ == "__main__":
     main()
