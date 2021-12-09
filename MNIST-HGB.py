fe8e8bbe4feeaf610d740b8e4ac54ec3884e9dcd
Alex Holt <alexholt54@gmail.com>
2021-12-09 03:03:47 -0800

    Updates

--- MNIST-HGB.py
+++ MNIST-HGB.py
@@ -13,99 +13,57 @@ def main():
 
     # Extract training set
     data = mnist["train_fea"]
+    data = normalizeData(data)
     label = mnist["train_gnd"]
-
-    data_lenet = mnistLeNet5["train_fea"]
-    label_lenet = mnistLeNet5["train_gnd"]
-
-    # Normalizing data
-    data = (data / 255)
-    train_mean = data.mean(axis = 0)
-    train_std = data.std(axis = 0)
-    train_std[train_std == 0] = 1
-    data -= train_mean
-    data /= train_std
     label = label.flatten()
 
-    data_lenet = (data_lenet / 255)
-    lenet_mean = data_lenet.mean(axis = 0)
-    lenet_std = data_lenet.std(axis = 0)
-    lenet_std[lenet_std == 0] = 1
-    data_lenet -= lenet_mean
-    data_lenet /= lenet_std
-    label_lenet = label_lenet.flatten()
+    lenet = mnistLeNet5["train_fea"]
+    lenet = normalizeData(lenet)
+    lenet_labels = mnistLeNet5["train_gnd"]
+    lenet_labels = lenet_labels.flatten()
 
-    # Split data into training and validation
-    num_training = 50000 # number of images to use in the training set
-
-    # Training set
-    data_train = data[:num_training]
-    labels_train = label[:num_training]
-
-    # Training set for lenet
-    lenet_train = data_lenet[:num_training]
-    lenet_labels = label_lenet[:num_training]
-
-    # Validation set will be 60000 - num_training
-    data_validation = data[num_training:]
-    labels_validation = label[num_training:]
-
-    # Validation set for lenet
-    lenet_validation = data_lenet[num_training:]
-    lenet_labels_validation = label_lenet[num_training:]
-
-    # Extract test set to use at the end
-    data_test = mnist["test_fea"]
-    label_test = mnist["test_gnd"]
+    # Extract Training and Validation Set
+    test = mnist["test_fea"]
+    test = normalizeData(test)
+    test_labels = mnist["test_gnd"]
+    test_labels = test_labels.flatten()
 
     lenet_test = mnistLeNet5["test_fea"]
-    lenet_label_test = mnistLeNet5["test_gnd"]
+    lenet_test = normalizeData(lenet_test)
+    lenet_test_labels = mnistLeNet5["test_gnd"]
+    lenet_test_labels = lenet_test_labels.flatten()
 
-    # Normalizing testing data
-    data_test = (data_test / 255)
-    test_mean = data_test.mean(axis = 0)
-    test_std = data_test.std(axis = 0)
-    test_std[test_std == 0] = 1
-    data_test -= test_mean
-    data_test /= test_std
-    label_test = label_test.flatten()
+    # Extract 7000 images for validation
+    validation = test[0:7000]
+    validation_labels = test_labels[0:7000]
 
-    lenet_test = (lenet_test / 255)
-    lenet_test_mean = lenet_test.mean(axis = 0)
-    lenet_test_std = lenet_test.std(axis = 0)
-    lenet_test_std[lenet_test_std == 0] = 1
-    lenet_test -= lenet_test_mean
-    lenet_test /= lenet_test_std
-    lenet_label_test = lenet_label_test.flatten()
+    lenet_validation = lenet_test[0:7000]
+    lenet_validation_labels = lenet_test_labels[0:7000]
 
-    # For MNIST:
-    # data_train, labels_train
-    # data_validation, labels_validation
-    # data_test, label_test
+    # Extract 3000 images for testing
+    test = test[7000:10000]
+    test_labels = test_labels[7000:10000]
 
-    # For MNIST LeNet5
-    # lenet_train, lenet_labels
-    # lenet_validation, lenet_labels_validation
-    # lenet_test, lenet_label_test
+    lenet_test = lenet_test[7000:10000]
+    lenet_test_lables = lenet_test_labels[7000:10000]
 
-    minTrees = 1
-    maxTrees = 500
+    minTrees = 100
+    maxTrees = 1000
 
-    trees = list(range(minTrees, maxTrees))
+    trees = list(range(minTrees, maxTrees, 100))
 
     valError = pd.DataFrame([], columns = ["trees", "error"])
     valErrorLenet = pd.DataFrame([], columns = ["trees", "error"])
 
     for tree in trees:
-        # Change parameters here
-        model = HistGradientBoostingClassifier(max_iter = tree).fit(data_train, labels_train)
-        lenet = HistGradientBoostingClassifier(max_iter = tree).fit(lenet_train, lenet_labels)
+        model = HistGradientBoostingClassifier(max_iter = tree).fit(data, label)
+        lenetModel = HistGradientBoostingClassifier(max_iter = tree).fit(lenet, lenet_labels)
 
-        row = {"trees" : tree, "error" : 1 - model.score(data_validation, labels_validation)}
-        lenet_row = {"trees" : tree, "error" : 1 - lenet.score(lenet_validation, lenet_labels_validation)}
+        row = {"trees" : tree, "error" : 1 - model.score(validation, validation_labels)}
+        lenet_row = {"trees" : tree, "error" : 1 - lenetModel.score(lenet_validation, lenet_validation_labels)}
 
         valError = valError.append(row, ignore_index=True)
-        valErrorLenet = valError.append(lenet_row, ignore_index=True)
+        valErrorLenet = valErrorLenet.append(lenet_row, ignore_index=True)
 
         print(tree)
 
@@ -114,6 +72,49 @@ def main():
                         title = "Validation Error With Varying Number of Trees", ylabel = "Validation Error", xlabel = "Number of Trees")
     plt.show()
 
+    minLearn = 0.1
+    maxLearn = 1
+
+    valError = pd.DataFrame([], columns = ["eta", "error"])
+    valErrorLenet = pd.DataFrame([], columns = ["eta", "error"])
+
+    # For MNIST:
+    # data, label
+    # validation, validation_labels
+    # test, test_lables
+
+    # For MNIST LeNet5
+    # lenet, lenet_labels
+    # lenet_validation, lenet_validation_labels
+    # lenet_test, lenet_test_labels
+
+    rates = list(range(minLearn, maxLearn, 0.1))
+    for rate in rates:
+        model = HistGradientBoostingClassifier(learning_rate = rate).fit(data, label)
+        lenetModel = HistGradientBoostingClassifier(learning_rate = rate).fit(lenet, lenet_labels)
+
+        row = {"eta" : rate, "error" : 1 - model.score(validation, validation_labels)}
+        lenet_row = {"eta" : rate, "error" : 1 - lenetModel.score(lenet_validation, lenet_validation_labels)}
+
+        valError = valError.append(row, ignore_index=True)
+        valErrorLenet = valErrorLenet.append(lenet_row, ignore_index=True)
+
+        print(rate)
+    ax = valError.plot(x = "eta", y = "error", kind = "line", color = "red", label = "Pixel Features")
+    valErrorLenet.plot(x = "eta", y = "error", kind = "line", ax = ax, color = "blue", label = "LeNet5 Features",
+                        title = "Validation Error With Varying Learning Rates", ylabel = "Validation Error", xlabel = "Learning Rate")
+    plt.show()
+
+
+
+def normalizeData(data):
+    data = (data / 255)
+    train_mean = data.mean(axis = 0)
+    train_std = data.std(axis = 0)
+    train_std[train_std == 0] = 1
+    data -= train_mean
+    data /= train_std
+    return data
 
 if __name__ == "__main__":
     main()
